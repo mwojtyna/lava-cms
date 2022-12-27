689d2825c10d17f82fe0e4608b7478ae5b033d76
Mateusz Wojtyna <w_mateusz@hotmail.com>
2022-12-28 00:41:23 +0100

    chore: set up frontend, admin, deployment

---
+++ www/admin/.eslintrc.cjs
@@ -0,0 +1,15 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: "./tsconfig.json",
+		tsconfigRootDir: __dirname,
+		sourceType: "module",
+	},
+	ignorePatterns: [".eslintrc.cjs"],
+	plugins: ["@typescript-eslint"],
+	extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+	rules: {
+		"@typescript-eslint/consistent-type-imports": "warn",
+	},
+};
