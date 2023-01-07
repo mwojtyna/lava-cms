604a81903771654b02a52b5d24cfe2c8ace1d77f
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-01-07 16:56:48 +0100

    feat: set up mantine

--- www/admin/app/auth/signin/page.tsx
+++ www/admin/app/auth/signin/page.tsx
@@ -1,13 +1,5 @@
 function One() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-			<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-				<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-					<span className="text-[hsl(280,100%,70%)]">Sign in</span>
-				</h1>
-			</div>
-		</main>
-	);
+	return <h1>Sign in</h1>;
 }
 
 export default One;
--- www/admin/app/layout.tsx
+++ www/admin/app/layout.tsx
@@ -1,4 +1,5 @@
-import "src/styles/globals.css";
+import React from "react";
+import RootStyleRegistry from "./emotion";
 
 export default function RootLayout({
 	children,
@@ -8,7 +9,9 @@ export default function RootLayout({
 	return (
 		<html>
 			<head />
-			<body>{children}</body>
+			<body>
+				<RootStyleRegistry>{children}</RootStyleRegistry>
+			</body>
 		</html>
 	);
 }
