a1b8fa3e89a26f49f1d1254460336e6f50b745c1
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-01-08 01:42:02 +0100

    feat: sign in/up form styling

--- www/admin/app/auth/signup/SignUpForm.tsx
+++ www/admin/app/auth/signup/SignUpForm.tsx
@@ -2,21 +2,22 @@
 
 import { Button, Group, TextInput, Title, UnstyledButton } from "@mantine/core";
 import { signIn } from "next-auth/react";
-import { type SubmitHandler, useForm } from "react-hook-form";
 import Link from "next/link";
+import { type SubmitHandler, useForm } from "react-hook-form";
 
-function SignInForm() {
+function SignUpForm() {
 	interface Inputs {
 		email: string;
 		password: string;
+		repeatPassword: string;
 	}
 
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
+		watch,
 	} = useForm<Inputs>({ mode: "onTouched" });
-
 	const onSubmit: SubmitHandler<Inputs> = (data) => {
 		signIn("credentials", {
 			callbackUrl: "/admin/dashboard",
@@ -30,7 +31,7 @@ function SignInForm() {
 			className="flex w-80 flex-col gap-4"
 		>
 			<Title order={1} variant="gradient">
-				Zaloguj się
+				Zarejestruj się
 			</Title>
 
 			<TextInput
@@ -40,8 +41,7 @@ function SignInForm() {
 				placeholder="user@domain.com"
 				{...register("email", {
 					required: true,
-					pattern:
-						/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+					pattern: /^\S+@\S+$/i,
 				})}
 				error={errors.email && "Niepoprawny adres e-mail!"}
 			/>
@@ -51,25 +51,40 @@ function SignInForm() {
 				label="Hasło"
 				{...register("password", {
 					required: true,
+					pattern:
+						/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
 				})}
-				error={!!errors.password}
+				error={
+					errors.password &&
+					"Hasło musi mieć minimum 8 znaków, jedną wielką literę, oraz jedną cyfrę!"
+				}
+			/>
+			<TextInput
+				size="md"
+				type="password"
+				label="Powtórz hasło"
+				{...register("repeatPassword", {
+					required: true,
+					validate: (value) => value === watch("password"),
+				})}
+				error={errors.repeatPassword && "Hasła nie są identyczne!"}
 			/>
 
 			<Group position="apart">
 				<UnstyledButton className="px-1">
 					<Link
-						href="/admin/auth/signup"
+						href="/admin/auth/signin"
 						className="text-inherit no-underline hover:underline"
 					>
-						Zarejestruj się
+						Zaloguj się
 					</Link>
 				</UnstyledButton>
 				<Button size="md" type="submit">
-					Zaloguj się
+					Zarejestruj się
 				</Button>
 			</Group>
 		</form>
 	);
 }
 
-export default SignInForm;
+export default SignUpForm;
---
+++ www/admin/app/auth/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from "./SignUpForm";
+
+function SignIn() {
+	return (
+		<div className="flex h-[100vh] items-center justify-center">
+			<SignUpForm />
+		</div>
+	);
+}
+
+export default SignIn;
