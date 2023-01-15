01e20cd37677b9391e0c85ac1521785827e5c195
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-01-15 02:03:09 +0100

    fix: production image

--- www/api/index.ts
+++ www/api/index.ts
@@ -2,6 +2,9 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import express from "express";
 import cors from "cors";
 import { appRouter } from "./trpc/routes/_router";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 4000;
