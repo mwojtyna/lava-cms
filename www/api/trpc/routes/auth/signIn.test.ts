84d0c5a4b8652f4a95952742a4713c6eea77a217
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-01-23 16:29:14 +0100

    test: signIn trpc procedure, add more test-related npm scripts

---
+++ www/api/trpc/routes/auth/signIn.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it, vi } from "vitest";
+import { prisma } from "@api/prisma/__mocks__/client";
+import bcrypt from "bcrypt";
+import { caller } from "../_router";
+
+vi.mock("@api/prisma/client");
+
+describe("signIn", () => {
+	const ID = "123456789abcdef";
+	const EMAIL = "johndoe@domain.com";
+	const PASSWORD = "password";
+
+	it.concurrent("returns user's id if email and password are correct", async () => {
+		prisma.users.findFirst.mockResolvedValue({
+			id: ID,
+			name: "John",
+			last_name: "Doe",
+			email: EMAIL,
+			password: await bcrypt.hash(PASSWORD, 10),
+		});
+		const userId = await caller.signIn({ email: EMAIL, password: PASSWORD });
+
+		expect(userId).toBe(ID);
+	});
+
+	it.concurrent("throws an error if email is incorrect", async () => {
+		prisma.users.findFirst.mockResolvedValue(null);
+
+		await expect(caller.signIn({ email: EMAIL, password: PASSWORD })).rejects.toThrow("email");
+	});
+
+	it.concurrent("throws an error if password is incorrect", async () => {
+		prisma.users.findFirst.mockResolvedValue({
+			id: ID,
+			name: "John",
+			last_name: "Doe",
+			email: EMAIL,
+			password: await bcrypt.hash(PASSWORD, 10),
+		});
+
+		await expect(caller.signIn({ email: EMAIL, password: "wrong password" })).rejects.toThrow(
+			"password"
+		);
+	});
+});
