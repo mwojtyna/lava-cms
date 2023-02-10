b453861db7c866aba09575f7c56188a619f1025e
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-02-11 00:14:45 +0100

    ci: playwright tests for firefox and webkit, vitest for api

--- www/api/vitest.config.ts
+++ www/api/vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	test: {
+		reporters: process.env.CI ? ["default", "html"] : ["default"],
+	},
 	resolve: {
 		alias: {
 			"@api": ".",
