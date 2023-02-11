f846941274a4f891c518471256018062d9177ec5
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-02-12 00:00:26 +0100

    feat: protect client-side trpc routes

--- www/admin/src/middleware.ts
+++ www/admin/src/middleware.ts
@@ -7,25 +7,33 @@ export default withAuth(
 		const url = request.nextUrl.clone();
 		const { firstTime } = await trpc.auth.firstTime.query();
 
-		if (firstTime && url.pathname !== "/auth/signup") {
-			// Redirect to sign up page if opening the dashboard for the first time
-			url.pathname = "/auth/signup";
-			return NextResponse.redirect(url);
-		}
-		if (!firstTime && url.pathname === "/auth/signup") {
-			// Redirect to dashboard if already signed up
-			url.pathname = "/dashboard";
-			return NextResponse.redirect(url);
-		}
-		if (!firstTime && !request.nextauth.token && url.pathname !== "/auth/signin") {
-			// Redirect to sign in page if not signed in
-			url.pathname = "/auth/signin";
-			return NextResponse.redirect(url);
-		}
-		if (!firstTime && request.nextauth.token && url.pathname === "/auth/signin") {
-			// Redirect to dashboard if already signed in
-			url.pathname = "/dashboard";
-			return NextResponse.redirect(url);
+		if (!url.pathname.startsWith("/api/trpc")) {
+			if (firstTime && url.pathname !== "/auth/signup") {
+				// Redirect to sign up page if opening the dashboard for the first time
+				url.pathname = "/auth/signup";
+				return NextResponse.redirect(url);
+			}
+			if (!firstTime && url.pathname === "/auth/signup") {
+				// Redirect to dashboard if already signed up
+				url.pathname = "/dashboard";
+				return NextResponse.redirect(url);
+			}
+			if (!firstTime && !request.nextauth.token && url.pathname !== "/auth/signin") {
+				// Redirect to sign in page if not signed in
+				url.pathname = "/auth/signin";
+				return NextResponse.redirect(url);
+			}
+			if (!firstTime && request.nextauth.token && url.pathname === "/auth/signin") {
+				// Redirect to dashboard if already signed in
+				url.pathname = "/dashboard";
+				return NextResponse.redirect(url);
+			}
+		} else {
+			// Return 401 if not signed in, there is a user in the db
+			// and trying to access the api
+			if (!request.nextauth.token && !firstTime) {
+				return new NextResponse(undefined, { status: 401 });
+			}
 		}
 
 		return NextResponse.next();
@@ -40,4 +48,4 @@ export default withAuth(
 	}
 );
 
-export const config = { matcher: ["/dashboard/:path*", "/auth/:path*"] };
+export const config = { matcher: ["/dashboard/:path*", "/auth/:path*", "/api/trpc/:path*"] };
