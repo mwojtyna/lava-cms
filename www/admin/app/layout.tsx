b2f1e678764380cad9a09b73374dc2306ec70f65
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-03-20 20:02:56 +0100

    feat: add lang attrib to admin html

--- www/admin/app/layout.tsx
+++ www/admin/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		?.value as ColorScheme;
 
 	return (
-		<html>
+		<html lang="en-US">
 			<body>
 				<TrpcProvider>
 					<Mantine colorScheme={colorScheme}>{children}</Mantine>
