ede0607aca4888c554b858f896869f93f5da63b0
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-03-22 17:37:57 +0100

    refactor: move side menu size to a constant

--- www/admin/app/dashboard/(components)/Menu.tsx
+++ www/admin/app/dashboard/(components)/Menu.tsx
@@ -4,33 +4,34 @@ import { Navbar, Drawer, MediaQuery, useMantineTheme } from "@mantine/core";
 import { useMenuStore } from "@admin/src/stores/dashboard";
 import MenuLinks from "./MenuLinks";
 
-export default function Menu({ version, serverUrl }: { version: string; serverUrl: string }) {
+export default function Menu({ version }: { version: string }) {
 	const menuStore = useMenuStore();
+	const WIDTH = 300;
 
 	return (
 		<>
 			<MediaQuery largerThan={"md"} styles={{ display: "none" }}>
 				<Drawer
-					size={300}
+					size={WIDTH}
 					withCloseButton={false}
 					opened={menuStore.isOpen}
 					onClose={() => menuStore.toggle()}
 					padding={0}
 				>
-					<MenuLinks version={version} serverUrl={serverUrl} />
+					<MenuLinks version={version} />
 				</Drawer>
 			</MediaQuery>
 
 			<MediaQuery smallerThan={"md"} styles={{ display: "none" }}>
 				<Navbar
 					sx={{
-						width: 300,
+						width: WIDTH,
 						flexShrink: 0,
 						height: "100vh",
 						overflow: "auto",
 					}}
 				>
-					<MenuLinks version={version} serverUrl={serverUrl} />
+					<MenuLinks version={version} />
 				</Navbar>
 			</MediaQuery>
 		</>
