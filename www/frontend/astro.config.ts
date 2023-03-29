a33e633e1decd91802b8787ad3a6365bec175129
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-03-29 17:14:52 +0200

    feat: switch astro to ssr using node adapter

--- www/frontend/astro.config.ts
+++ www/frontend/astro.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
 	server: {
 		host: true,
 	},
+	output: "server",
+	adapter: node({
+		mode: "standalone",
+	}),
 });
