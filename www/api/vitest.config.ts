2d88dc746e213f4fd0f2050f6fe3a0595c850c39
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-04-01 18:30:51 +0200

    refactor: change component test extension to .ts

--- www/api/vitest.config.ts
+++ www/api/vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		reporters: process.env.CI ? ["default", "html"] : ["default"],
+		dir: ".",
 	},
 	resolve: {
 		alias: {
