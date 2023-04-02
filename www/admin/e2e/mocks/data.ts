f9ca6064e99ff7e9b24fc34f2d0ba965d57fd9ea
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-04-02 16:32:51 +0200

    fix (tests): mismatch between test created user id and user id saved in cookie

--- www/admin/e2e/mocks/data.ts
+++ www/admin/e2e/mocks/data.ts
@@ -1,6 +1,7 @@
 import type { User, Config } from "api/prisma/types";
 
-export const userMock: Omit<User, "id"> = {
+export const userMock: User = {
+	id: "clfykj2350000nzjhb7hrvan4",
 	name: "John",
 	last_name: "Doe",
 	email: "johndoe@domain.com",
