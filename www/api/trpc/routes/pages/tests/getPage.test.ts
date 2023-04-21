3937bcb748be91b5aa82df6f2fc9e02f3703e003
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-04-21 16:11:42 +0200

    test: add test for page.getPage endpoint

---
+++ www/api/trpc/routes/pages/tests/getPage.test.ts
@@ -0,0 +1,20 @@
+import { expect, it, vi } from "vitest";
+import { prisma } from "@api/prisma/__mocks__/client";
+import { caller } from "@api/trpc/routes/_app";
+
+vi.mock("@api/prisma/client");
+
+const PAGE = {
+	id: "0",
+	name: "Test",
+	url: "/test",
+	order: 0,
+	parent_id: null,
+};
+
+it("should return a page", async () => {
+	prisma.page.findUnique.mockResolvedValueOnce(PAGE);
+
+	const page = await caller.pages.getPage({ url: PAGE.url });
+	expect(page).toEqual(PAGE);
+});
