8f1b41dc8a24855508fad84ab907aac0fc644d7e
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-05-05 23:49:52 +0000

    fix: warnings when saving src/components/ui/client/index.ts

--- www/admin/src/components/ui/client/index.ts
+++ www/admin/src/components/ui/client/index.ts
@@ -1,7 +1,7 @@
-export * from "./ActionIcon";
-export * from "./Button";
-export * from "./Input";
-export * from "./Label";
-export * from "./Switch";
-export * from "./Textarea";
-export * from "./Tooltip";
+export { ActionIcon } from "./ActionIcon";
+export { Button, buttonVariants } from "./Button";
+export { Input, type InputBaseProps, InputWrapper } from "./Input";
+export { Label } from "./Label";
+export { Switch } from "./Switch";
+export { Textarea } from "./Textarea";
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./Tooltip";
