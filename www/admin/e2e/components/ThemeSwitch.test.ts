720fb11fb3655b7d824a18f14b2a1187b778fc2e
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-06-17 14:01:06 +0000

    test: update tests for ThemeSwitch component

--- www/admin/e2e/components/ThemeSwitch.test.ts
+++ www/admin/e2e/components/ThemeSwitch.test.ts
@@ -1,11 +1,27 @@
-import { test, expect, type Page } from "@playwright/test";
+import { test, expect, type Page, type BrowserContext } from "@playwright/test";
 import { init } from "api/server";
 import { start, stop } from "../mocks/trpc";
+import { type ColorTheme, colorThemeSchema } from "@admin/src/data/stores/dashboard";
+import type { CookieName } from "@admin/src/utils/cookies";
 
 const TEST_ID = "theme-switch";
-function getColorScheme(page: Page) {
-	return page.locator("html").evaluate((node) => window.getComputedStyle(node).colorScheme);
-}
+
+const getColorScheme = (page: Page): Promise<ColorTheme> =>
+	page.locator("body").evaluate((node) => (node.classList.contains("dark") ? "dark" : "light"));
+
+const getCookie = (context: BrowserContext): Promise<ColorTheme | undefined> =>
+	context
+		.cookies()
+		.then(
+			async (cookies) =>
+				await colorThemeSchema
+					.optional()
+					.parseAsync(
+						cookies.find(
+							(cookie) => cookie.name === ("color-theme" satisfies CookieName)
+						)?.value
+					)
+		);
 
 test.beforeAll(async () => {
 	await start(await init());
@@ -15,50 +31,101 @@ test.afterAll(async () => {
 });
 
 test("light theme visual comparison", async ({ page }) => {
-	await page.goto("/admin");
+	await page.goto("/admin/setup");
 	expect(await page.getByTestId(TEST_ID).screenshot()).toMatchSnapshot();
 });
 test("dark theme visual comparison", async ({ page }) => {
-	await page.goto("/admin");
+	await page.goto("/admin/setup");
 	const element = page.getByTestId(TEST_ID);
+
 	await element.click();
-	await page.waitForTimeout(500); // Wait for the switch to animate
+	await page.waitForTimeout(100); // Wait for the switch to animate
 
 	expect(await element.screenshot()).toMatchSnapshot();
 });
 
-test("automatically switches to light theme if it is the preferred theme and cookie not is present", async ({
-	page,
-}) => {
+test("when pressing the switch, cookie and theme are updated", async ({ page, context }) => {
+	expect(await getCookie(context)).toBeUndefined();
+
 	await page.emulateMedia({ colorScheme: "light" });
-	await page.goto("/admin", { waitUntil: "networkidle" });
+	await page.goto("/admin/setup", { waitUntil: "networkidle" });
+
+	const themeSwitch = page.getByTestId(TEST_ID);
+
+	expect(await getColorScheme(page)).toBe("light");
+	expect(await getCookie(context)).toBe("light");
+
+	await themeSwitch.click();
+	await page.waitForTimeout(100); // Wait for the switch to animate
+
+	expect(await getColorScheme(page)).toBe("dark");
+	expect(await getCookie(context)).toBe("dark");
+
+	await themeSwitch.click();
+	await page.waitForTimeout(100); // Wait for the switch to animate
+
+	expect(await getColorScheme(page)).toBe("light");
+	expect(await getCookie(context)).toBe("light");
+});
+
+test("automatically switches to light theme and sets cookie if it is the preferred theme and cookie is not present", async ({
+	page,
+	context,
+}) => {
+	expect(await getCookie(context)).toBe(undefined);
+
+	await page.emulateMedia({ colorScheme: "light" });
+	await page.goto("/admin/setup", { waitUntil: "networkidle" });
 
 	const colorScheme = await getColorScheme(page);
 	expect(colorScheme).toBe("light");
+	expect(await getCookie(context)).toBe("light");
 });
-test("automatically switches to dark theme if it is the preferred theme and cookie not is present", async ({
+test("automatically switches to dark theme and sets cookie if it is the preferred theme and cookie is not present", async ({
 	page,
+	context,
 }) => {
+	expect(await getCookie(context)).toBe(undefined);
+
 	await page.emulateMedia({ colorScheme: "dark" });
-	await page.goto("/admin", { waitUntil: "networkidle" });
+	await page.goto("/admin/setup", { waitUntil: "networkidle" });
 
 	const colorScheme = await getColorScheme(page);
 	expect(colorScheme).toBe("dark");
+	expect(await getCookie(context)).toBe("dark");
 });
 
-test("reads color scheme from cookie if present", async ({ page, context }) => {
+test("when changing the preferred system theme, the website theme is updated", async ({
+	page,
+	context,
+}) => {
+	await page.emulateMedia({ colorScheme: "light" });
+	await page.goto("/admin/setup", { waitUntil: "networkidle" });
+
+	expect(await getColorScheme(page)).toBe("light");
+	expect(await getCookie(context)).toBe("light");
+
 	await page.emulateMedia({ colorScheme: "dark" });
-	await page.goto("/admin", { waitUntil: "networkidle" });
 
-	const initialColorScheme = await getColorScheme(page);
-	expect(initialColorScheme).toBe("dark");
+	expect(await getColorScheme(page)).toBe("dark");
+	expect(await getCookie(context)).toBe("dark");
+});
+test("when cookie disagrees with preferred theme, cookie takes precedence", async ({
+	page,
+	context,
+}) => {
+	await context.addCookies([
+		{
+			name: "color-theme" satisfies CookieName,
+			value: "dark",
+			sameSite: "Lax",
+			domain: "localhost",
+			path: "/",
+		},
+	]);
+	await page.emulateMedia({ colorScheme: "light" });
+	await page.goto("/admin/setup", { waitUntil: "networkidle" });
 
-	await page.getByTestId(TEST_ID).click();
-	await page.reload({ waitUntil: "networkidle" });
-
-	const cookiesColorScheme = await getColorScheme(page);
-	expect(cookiesColorScheme).toBe("light");
-	expect((await context.cookies()).find((cookie) => cookie.name === "color-scheme")?.value).toBe(
-		"light"
-	);
+	expect(await getColorScheme(page)).toBe("dark");
+	expect(await getCookie(context)).toBe("dark");
 });
