9920c1dc782a782931c30661932d270647ca7c99
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-07-03 19:16:45 +0000

    chore: update to next 13.4.8, improve dev server performance with modularizeImports

--- www/admin/next.config.mjs
+++ www/admin/next.config.mjs
@@ -7,6 +7,20 @@ const config = {
 	basePath: "/admin",
 	reactStrictMode: true,
 	swcMinify: true,
+	modularizeImports: {
+		"@heroicons/react/24/outline": {
+			transform: "@heroicons/react/24/outline/{{member}}",
+		},
+		"@heroicons/react/24/solid": {
+			transform: "@heroicons/react/24/solid/{{member}}",
+		},
+		"@heroicons/react/20/solid": {
+			transform: "@heroicons/react/20/solid/{{member}}",
+		},
+		"@tabler/icons-react": {
+			transform: "@tabler/icons-react/dist/esm/icons/{{member}}",
+		},
+	},
 
 	redirects: async () => [
 		{
