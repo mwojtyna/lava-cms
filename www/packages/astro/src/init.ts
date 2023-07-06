ef5fd5fefbc8222e2e47457031855274bc55ee53
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-07-06 15:45:47 +0000

    docs: add docs for initApi function

--- www/packages/astro/src/init.ts
+++ www/packages/astro/src/init.ts
@@ -3,10 +3,21 @@ import type { PublicRouter } from "@lavacms/types";
 import SuperJSON from "superjson";
 
 interface InitApiConfig {
+	/**
+	 * Base URL of your self-hosted CMS
+	 * @example "https://lavacms.com/admin"
+	 */
 	url: string;
+	/** Token copied from your CMS connection settings page */
 	token: string;
+	/** Log requests and responses to console */
 	log?: boolean;
 }
+
+/**
+ * Initialize the connection to your CMS
+ * @returns A TRPC client that can be used to make requests to your CMS
+ */
 export const initApi = (config: InitApiConfig) =>
 	createTRPCProxyClient<PublicRouter>({
 		links: [
