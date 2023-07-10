4ff392a9fb6296d348b76e3a9db9b573907fea98
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-07-10 22:07:06 +0000

    refactor: use proper types for astro components

--- www/packages/astro/index.ts
+++ www/packages/astro/index.ts
@@ -4,6 +4,4 @@ export { useLavaCms, ApiClient } from "@lavacms/core";
 import type { LavaCms as LavaCmsCore } from "@lavacms/core";
 export namespace LavaCms {
 	export type Component = LavaCmsCore.Component;
-	export type ComponentData = LavaCmsCore.ComponentData;
-	export type ContentType = LavaCmsCore.ContentType;
 }
