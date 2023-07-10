653586abf77acdf9a6bfe1c2687f422a2b2fa4f8
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-07-10 21:44:50 +0000

    chore: remove unnecessary type exports in @lavacms/types

--- www/packages/types/index.ts
+++ www/packages/types/index.ts
@@ -1,4 +1,1 @@
 export type { PublicRouter } from "@admin/src/trpc/routes/public/_public";
-export type { Meta } from "@admin/src/trpc";
-export type * from "@trpc/server/dist";
-export type * from "@prisma/client/runtime";
