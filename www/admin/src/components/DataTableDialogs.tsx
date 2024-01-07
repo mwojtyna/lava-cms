ac7404093f6c0c825bcdf11eb20d057e56c1bedb
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-01-07 16:53:17 +0100

    fix: NewParentSelect was given ref but didn't accept one

--- www/admin/src/components/DataTableDialogs.tsx
+++ www/admin/src/components/DataTableDialogs.tsx
@@ -1,4 +1,5 @@
 import type { FormFieldProps } from "./ui/client";
+import { forwardRef, type ComponentRef } from "react";
 import { Combobox } from "./Combobox";
 import { TypographyMuted } from "./ui/server";
 
@@ -14,29 +15,33 @@ export interface ItemParent {
 interface NewParentSelectProps extends FormFieldProps<string> {
 	parents: ItemParent[];
 }
-export function NewParentSelect(props: NewParentSelectProps) {
-	return (
-		<Combobox
-			className="w-full"
-			contentProps={{
-				align: "center",
-				className: "min-w-[335px] w-fit max-w-[97.5vw] max-h-[47.5vh] overflow-auto",
-				placeholder: "Search groups...",
-			}}
-			placeholder="Select a group..."
-			notFoundContent="No groups found."
-			data={
-				props.parents.map((group) => ({
-					label: <span className="flex items-baseline gap-2">{group.name} </span>,
-					description: (
-						<TypographyMuted className="text-xs">{group.extraInfo}</TypographyMuted>
-					),
-					value: group.id,
-					filterValue: group.name,
-				})) ?? []
-			}
-			aria-required
-			{...props}
-		/>
-	);
-}
+export const NewParentSelect = forwardRef<ComponentRef<typeof Combobox>, NewParentSelectProps>(
+	(props, ref) => {
+		return (
+			<Combobox
+				ref={ref}
+				className="w-full"
+				contentProps={{
+					align: "center",
+					className: "min-w-[335px] w-fit max-w-[97.5vw] max-h-[47.5vh] overflow-auto",
+					placeholder: "Search groups...",
+				}}
+				placeholder="Select a group..."
+				notFoundContent="No groups found."
+				data={
+					props.parents.map((group) => ({
+						label: <span className="flex items-baseline gap-2">{group.name} </span>,
+						description: (
+							<TypographyMuted className="text-xs">{group.extraInfo}</TypographyMuted>
+						),
+						value: group.id,
+						filterValue: group.name,
+					})) ?? []
+				}
+				aria-required
+				{...props}
+			/>
+		);
+	},
+);
+NewParentSelect.displayName = "NewParentSelect";
