a68317d4da5308123db72a89d1fce93474096bd6
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-02-06 21:37:00 +0100

    fix(rich-text): FixedToolbarButtons stuck to low when scrolling

--- www/admin/src/components/plate-ui/FixedToolbar.tsx
+++ www/admin/src/components/plate-ui/FixedToolbar.tsx
@@ -4,5 +4,5 @@ import { Toolbar } from "./Toolbar";
 
 export const FixedToolbar = withCn(
 	Toolbar,
-	"supports-backdrop-blur:bg-background/60 sticky left-0 top-[57px] z-50 w-full justify-between overflow-x-auto rounded-t-lg border border-border bg-background/95 backdrop-blur",
+	"supports-backdrop-blur:bg-background/60 sticky left-0 -top-4 z-50 w-full justify-between overflow-x-auto rounded-t-lg border border-border bg-background/95 backdrop-blur",
 );
