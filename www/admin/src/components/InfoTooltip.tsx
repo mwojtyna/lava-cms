d61043449631ba054658470d5af6ab450c655f78
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-03-02 21:56:53 +0100

    refactor(InfoTooltip): use tooltip instead of a popover

--- www/admin/src/components/InfoTooltip.tsx
+++ www/admin/src/components/InfoTooltip.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import type { PopoverTrigger } from "./ui/client/Popover";
 import { QuestionMarkCircleIcon } from "@heroicons/react/20/solid";
 import { type VariantProps, cva } from "class-variance-authority";
 import * as React from "react";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/client/Popover";
+import { cn } from "../utils/styling";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/client/Tooltip";
 
 interface InfoTooltipProps
-	extends React.ComponentPropsWithoutRef<typeof Popover>,
+	extends React.ComponentPropsWithoutRef<typeof Tooltip>,
 		VariantProps<typeof variants> {
+	className?: string;
 	iconClassName?: string;
 }
 
@@ -24,13 +27,13 @@ const variants = cva("", {
 });
 
 const InfoTooltip = React.forwardRef<React.ComponentRef<typeof PopoverTrigger>, InfoTooltipProps>(
-	({ iconClassName, size, children }, ref) => (
-		<Popover>
-			<PopoverTrigger ref={ref} className="cursor-help" aria-label="More information">
+	({ className, iconClassName, size, children }, ref) => (
+		<Tooltip>
+			<TooltipTrigger ref={ref} className="cursor-help" aria-label="More information">
 				<QuestionMarkCircleIcon className={variants({ className: iconClassName, size })} />
-			</PopoverTrigger>
-			<PopoverContent variant={"tooltip"}>{children}</PopoverContent>
-		</Popover>
+			</TooltipTrigger>
+			<TooltipContent className={cn("font-normal", className)}>{children}</TooltipContent>
+		</Tooltip>
 	),
 );
 InfoTooltip.displayName = "InfoTooltip";
